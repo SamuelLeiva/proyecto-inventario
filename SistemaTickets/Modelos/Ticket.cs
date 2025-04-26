6d5e63756361594120909ecc6a1b711f417fcf44
SamuelLeiva <leiva20152@gmail.com>
2025-04-25 21:57:17 -0500

    Ticket class creada

--- SistemaTickets/Modelos/Ticket.cs
+++ SistemaTickets/Modelos/Ticket.cs
@@ -8,4 +8,22 @@ namespace SistemaTickets.Modelos;
 
 public class Ticket
 {
+    private static int _nextId = 1;
+
+    public int Id { get; private set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TicketStatus Status { get; set; } = TicketStatus.Open; //Por defecto el ticket empieza como activo (abierto)
+    public Priority Priority { get; set; }
+    public TicketCategory Category { get; set; }
+    public string ReportedBy { get; set; }
+    public DateTime CreatedDate { get; private set; } = DateTime.Now;
+    public DateTime? LastUpdated { get; set; }
+    public TeamMember? AssignedTo { get; set; }
+    public List<Comment> Comments { get; private set; } = new List<Comment>();
+
+    public Ticket()
+    {
+        Id = _nextId++;
+    }
 }
