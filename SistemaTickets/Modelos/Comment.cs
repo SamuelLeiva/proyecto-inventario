43c43d1d7eef48eee46d23261118e7f9cbc15fa6
SamuelLeiva <leiva20152@gmail.com>
2025-04-25 21:49:46 -0500

    Comment class creada

--- SistemaTickets/Modelos/Comment.cs
+++ SistemaTickets/Modelos/Comment.cs
@@ -8,4 +8,14 @@ namespace SistemaTickets.Modelos;
 
 public class Comment
 {
+    public int Id { get; private set; }
+    public string Author { get; private set; }
+    public string Text { get; private set; }
+    public DateTime CreatedDate { get; private set; } = DateTime.Now;
+
+    public Comment(string author, string text)
+    {
+        Author = author;
+        Text = text;
+    }
 }
