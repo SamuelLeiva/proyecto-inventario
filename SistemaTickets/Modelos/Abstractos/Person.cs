43b69fbe9b7c81cc2010f3c14ad4fb5c14974edd
SamuelLeiva <leiva20152@gmail.com>
2025-04-25 22:01:28 -0500

    Clase abstracta Person que hereda hacia Developer

---
+++ SistemaTickets/Modelos/Abstractos/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTickets.Modelos.Abstractos;
+
+public abstract class Person
+{
+    public string Name { get; protected set; }
+    public string Genre { get; protected set; }
+    public string Dni { get; protected set; }
+    public string Adress { get; protected set; }
+    public string Age { get; protected set; }
+}
--- SistemaTickets/Modelos/Developer.cs
+++ SistemaTickets/Modelos/Developer.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaTickets.Modelos.Abstractos;
 
 namespace SistemaTickets.Modelos;
 
-public class Developer
+public class Developer : Person
 {
     private static int _nextId = 1;
 
     public int Id { get; private set; }
-    public string Name { get; set; }
     public string Role { get; set; }
     public List<Ticket> AssignedTickets { get; private set; } = new List<Ticket>();
 
